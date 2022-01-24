b394fe32cf77de78d449100f910cebcaa3ffe7d6
Dave Johnson <djohnson@apollographql.com>
2022-01-24 10:52:22 -0500

    Adding schema check scripts.

--- scripts/check-astronauts.sh
+++ scripts/check-astronauts.sh
@@ -1,7 +1,7 @@
 
 rover graph introspect ${ASTRONAUTS_URL} | rover graph check ${APOLLO_GRAPH_REF} --schema -
-rover graph introspect ${ASTRONAUTS_URL} | rover subgraph check ${APOLLO_GRAPH_REF} --schema -
+rover graph introspect ${ASTRONAUTS_URL} | rover subgraph check ${APOLLO_GRAPH_REF} --name astronauts --schema -
 
 rover graph introspect ${MISSION_URL} | rover graph check ${APOLLO_GRAPH_REF} --schema -
-rover graph introspect ${MISSION_URL} | rover subgraph check ${APOLLO_GRAPH_REF} --schema -
+rover graph introspect ${MISSION_URL} | rover subgraph check ${APOLLO_GRAPH_REF} --name mission --schema -
 
