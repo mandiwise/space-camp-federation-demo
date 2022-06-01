07988828792705c24c197e164dd369e675abb60e
Dave Johnson <djohnson@apollographql.com>
2022-06-01 16:14:46 -0400

    arbitrary change

--- scripts/check-mission.sh
+++ scripts/check-mission.sh
@@ -1,4 +1,3 @@
 
 rover graph introspect ${MISSION_URL} | rover graph check ${APOLLO_GRAPH_REF} --schema -
 rover graph introspect ${MISSION_URL} | rover subgraph check ${APOLLO_GRAPH_REF} --name spacemission --schema -
-
